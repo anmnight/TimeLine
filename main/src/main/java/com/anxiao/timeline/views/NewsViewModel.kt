1e7bc75c78edc205a82738c877cd983a70fe4e61
anxiao <anmnight@qq.com>
2020-06-09 17:18:13 +0800

    add Result parser

--- main/src/main/java/com/anxiao/timeline/views/NewsViewModel.kt
+++ main/src/main/java/com/anxiao/timeline/views/NewsViewModel.kt
@@ -3,17 +3,33 @@ package com.anxiao.timeline.views
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.anxiao.timeline.data.NewsRepo
+import com.anxiao.timeline.data.database.dao.NewsDao
+import com.anxiao.timeline.data.network.RemoteService
+import com.anxiao.timeline.expand.checkResult
+import kotlinx.coroutines.launch
 
 class NewsViewModel : ViewModel() {
 
-    private var repo: NewsRepo = NewsRepo()
+    private lateinit var dao: NewsDao
+    private lateinit var api: RemoteService
+    private lateinit var newsRepo: NewsRepo
 
 
 
+    fun loadNews() =
+        viewModelScope.launch {
+            val result = newsRepo.getNews()
 
+            result.checkResult(
+                onSuccess = {
+                    println("")
+                },
+                onError = {
+                    println(it)
+                }
+            )
+        }
 
-    fun test(){
-    }
 
 }
 
